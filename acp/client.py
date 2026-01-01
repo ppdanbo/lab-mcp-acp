39dbbd4af66ba739269d40f37a6c0102e813fe6d
Danbo <danbowww@gmail.com>
2026-01-01 18:11:13 -0500

    acp client and server

--- acp/client.py
+++ acp/client.py
@@ -5,10 +5,18 @@ from acp_sdk.client import Client
 from acp_sdk.models import Message, MessagePart
 
 async def example() -> None:
-    async with Client(base_url="http://localhost:9000") as client:
-        run = await client.run_sync(agent="Greetings", input=Message(parts=[MessagePart(content="Aref")]))
-        print(run.output[0].parts[0].content)
-
+    async with Client(base_url="http://localhost:8000") as client:
+        # run = await client.run_sync(agent="Greetings", input=Message(parts=[MessagePart(content="Danbo")]))
+        # print(run.output[0].parts[0].content)
+        user_msg = Message(
+            role="user", 
+            parts=[MessagePart(content="Entropy")]
+            )
+        result = await client.run_sync(
+            agent="MeaningAgent", 
+            input=[user_msg]
+        )
+        print(f"Client output: {result.output[-1].parts[0].content}")
 
 if __name__ == "__main__":
     asyncio.run(example())
--- acp/server.py
+++ acp/server.py
@@ -12,15 +12,21 @@ dotenv.load_dotenv()
 server = Server()
 
 @server.agent("MeaningAgent")
-async def meaning(words: Message) -> str:
-    """A tool function that accepts a parameter called word and returns its meaning."""
+async def meaning(messages: list[Message]) -> str:
+    """A agent function that extract text from last user message and returns its meaning."""
 
+    user_input = messages[-1].parts[0].content  # Get the content of the last message from the user    
     #supported llm names are "ollama", "openai", "watsonx", "groq", "xai", "vertexai", "amazon_bedrock", "anthropic", "azure_openai", "mistralai"
     llm= ChatModel.from_name("openai:" + os.getenv("OPENAI_MODEL"))
-
+    
     memory = TokenMemory(llm)
-    agent = ReActAgent(llm=llm, tools=[], memory=memory)
-    response = await agent.run(prompt=f"Find the meaning of the word: {words[0]}. Be concise and return one sentence.",)
-    return response.result.text
+    agent = ReActAgent(llm=llm, 
+                       tools=[], 
+                       memory=memory)
+    
+    response = await agent.run(f"Find the meaning of the word: {user_input}. Be concise and return one sentence.",)
+    final_answer = response.iterations[-1].state.final_answer
+    print(f"\nFinal Answer: {final_answer}")
+    return final_answer
 
 server.run() 
