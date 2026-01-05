01d79c5e42bedd2ecac004b4716754aee96491b2
Danbo <danbowww@gmail.com>
2026-01-05 18:23:24 -0500

    acp ssl prep

--- acp/simple-client.py
+++ acp/simple-client.py
@@ -1,4 +1,3 @@
-
 import asyncio
 from acp_sdk.client import Client
 from acp_sdk.models import Message , MessagePart
@@ -6,8 +5,8 @@ from acp_sdk.models import Message , MessagePart
 
 async def example() -> None:
     async with Client(base_url="http://localhost:8090") as client:
-        #run = await client.run_sync(agent="NameAgent", input=Message(parts=[MessagePart(content="Danbo")]))
-        run = await client.run_sync(agent="NameAgent", input="Danbo")
+        #run = await client.run_sync(agent="NameAgent", input=Message(parts=[MessagePart(content="Aref")]))
+        run = await client.run_sync(agent="NameAgent", input="Aref")
         print(run)
         print(f"\n{run.output[0].parts[0].content}")
 
--- acp/ssl/README.md
+++ acp/ssl/README.md
@@ -1,9 +1,15 @@
-### how to create ssl key and crt
-REM Generate unencrypted private key
+### How to create ssl key and crt
+#### Generate unencrypted private key
+``` bash
 openssl genrsa -out localhost.key 2048
+```
 
-REM Create certificate signing request (CSR)
+#### Create certificate signing request (CSR)
+``` bash
 openssl req -new -key localhost.key -out localhost.csr -subj "/CN=localhost"
+```
 
-REM Generate self-signed certificate
+#### Generate self-signed certificate
+``` bash
 openssl x509 -req -days 365 -in localhost.csr -signkey localhost.key -out localhost.crt
+```
